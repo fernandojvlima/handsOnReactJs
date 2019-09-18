a537ab99f9ce18fe45dfab4845baa318e8835713
Fernando Lima <fernandojvlima@gmail.com>
2019-09-18 17:52:26 -0300

    Uploading part of class 2

--- minhas-series/src/App.js
+++ minhas-series/src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, {useEffect ,useState} from 'react';
+import Header from './Header';
+import axios from 'axios';
 import {
-  Navbar, 
-  NavbarBrand, 
-  Collapse, 
-  Nav,
-  NavItem,
-  NavLink,
-  NavbarToggler
-} from 'reactstrap';
+  BrowserRouter as Router, 
+  Route
+} from 'react-router-dom';
+
+const Home = () => {
+  return <h1>Home</h1>
+}
+
+const Genres = () => {
+  return <h1>Gêneros</h1>
+}
+
 
 function App() {
+  useEffect(() =>{
+    console.log(1);
+  }, []);
   return (
-    <div>
-        <Navbar color='light' light expand='md'>
-          <NavbarBrand>Minhas Séries</NavbarBrand> 
-          <Collapse isOpen={true}> </Collapse> 
-
-        </Navbar> 
-    </div>
-  );
+    <Router>
+      <div>
+        <Header />
+        <Route path='/' exact component={Home} />
+        <Route path='/generos' component={Genres}/>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
