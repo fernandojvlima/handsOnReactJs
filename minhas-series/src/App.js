1f3fbfaa05278c7056df220d8c3fd1e4a693e966
Fernando Lima <fernandojvlima@gmail.com>
2019-10-03 20:16:28 -0300

    class hands on

--- minhas-series/src/App.js
+++ minhas-series/src/App.js
@@ -1,33 +1,29 @@
 import React, {useEffect ,useState} from 'react';
-import Header from './Header';
+
 import axios from 'axios';
-import {
-  BrowserRouter as Router, 
-  Route
-} from 'react-router-dom';
+
 
 const Home = () => {
   return <h1>Home</h1>
 }
 
-const Genres = () => {
-  return <h1>Gêneros</h1>
-}
+// const Genres = () => {
+//   return <h1>Gêneros</h1>
+// }
 
-
-function App() {
-  useEffect(() =>{
-    console.log(1);
-  }, []);
-  return (
-    <Router>
-      <div>
-        <Header />
-        <Route path='/' exact component={Home} />
-        <Route path='/generos' component={Genres}/>
-      </div>
-    </Router>
-  )
-}
+// function App() {
+//   useEffect(() =>{
+//     console.log(1);
+//   }, []);
+//   return (
+//     <Router>
+//       <div>
+//         <Header />
+//         <Route path='/' exact component={Home} />
+//         <Route path='/generos' component={Genres}/>
+//       </div>
+//     </Router>
+//   )
+// }
 
 export default App;
